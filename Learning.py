795a6bb75e29455ad1223c8d9e5e38f41bde5f3f
Sebastian Pawlak <Sebastian.Pawlak@it.integro.pl>
2017-06-14 00:59:23 +0200

    sldkfj

--- Learning.py
+++ Learning.py
@@ -5,16 +5,23 @@ seed = 7
 np.random.seed(seed)
 
 
-def learn(images_dir, labes_dir):
-    data = DataLoader.get_images_names_list(images_dir, labes_dir)
+def learn(images_dir, labels_dir, batch_size):
+    data = DataLoader.get_images_names_list(images_dir, labels_dir)
+    train_pictures = []
+    labels = []
     for i in range(len(data)):
         print("pic num: {0}, name: {1}".format(i, data[i][0]))
+        train_pictures.append(DataLoader.get_image(data[i][1]))
+        labels.append(DataLoader.get_image(data[i][2]))
+        if i % batch_size == 0 and i>0:
+            print("learning")
+
 
 
 def main():
     learning_files_path = 'D:/Users/sebastian.pawlak/Downloads/pliki'
     learning_labels_path = 'D:/Users/sebastian.pawlak/Downloads/pliki'
-
+    batch_size = 10
     learn(learning_files_path, learning_labels_path)
 
 
--- Model.py
+++ Model.py
@@ -1,24 +1,54 @@
 from keras.models import Sequential
 from keras.layers.core import Dense
 from keras.layers.normalization import BatchNormalization
+from keras.layers import Deconvolution2D, Convolution2D, MaxPooling2D, Dropout, UpSampling2D
+from keras.preprocessing.image import ImageDataGenerator
+from sklearn.utils import shuffle
+
 import numpy as np
 
-class Model:
-    def __init__(self, gamma, actions_count, states_count, hidden_nodes_count):
-        self.gamma = gamma
-        self.states_count = states_count
-        self.actions_count = actions_count
-        self.hidden_nodes_number = hidden_nodes_count
 
-        self.states = 1
+class Model:
+    def __init__(self, gamma, pictures):
+        self.gamma = gamma
+
+        self.batch_size = 150
+        self.epochs = 20
+        self.pool_size = (2, 2)
+        self.input_shape = pictures.shape[1:]
 
         self.model = Sequential()
-        self.model.add(BatchNormalization(input_shape=input_shape))
+        self.model.add(BatchNormalization(input_shape=self.input_shape))
+        self.model.add(Convolution2D(60, 3, 3, border_mode='valid', subsample=(1, 1), activation='relu'))
+        self.model.add(Convolution2D(50, 3, 3, border_mode='valid', subsample=(1, 1), activation='relu'))
+        self.model.add(MaxPooling2D(pool_size=self.pool_size))
+        self.model.add(Convolution2D(40, 3, 3, border_mode='valid', subsample=(1, 1), activation='relu'))
+        self.model.add(Dropout(0.2))
+        self.model.add(Deconvolution2D(40, 3, 3, border_mode='valid', subsample=(1, 1), activation='relu',
+                                       output_shape=self.model.layers[4].output_shape))
+        self.model.add(Dropout(0.2))
+        self.model.add(UpSampling2D(size=self.pool_size))
+        self.model.add(Deconvolution2D(50, 3, 3, border_mode='valid', subsample=(1, 1), activation='relu',
+                                       output_shape=self.model.layers[3].output_shape))
+        self.model.add(Deconvolution2D(60, 3, 3, border_mode='valid', subsample=(1, 1), activation='relu',
+                                       output_shape=self.model.layers[1].output_shape))
+        self.model.add(Deconvolution2D(1, 3, 3, border_mode='valid', subsample=(1, 1), activation='relu',
+                                       output_shape=self.model.layers[0].output_shape))
 
-        self.exp_replay = ExperienceReplay(self.actions_count, self.states_count, max_memory=max_memory, discount=self.gamma)
 
-    def remember(self, state_before_move, action, reward, state_after_move):
-        self.exp_replay.remember([np.array([state_before_move]), action, reward, np.array([state_after_move])])
+        self.model.compile(optimizer='Adam', loss='mean_squared_error')
 
-    def clear_session(self):
-        kerasBackend.clear_session()
+    def train_model(self, pictures, labels):
+        labels = labels / 255
+        train_images, labels = shuffle(pictures, labels)
+        datagen = ImageDataGenerator()
+        datagen.fit(pictures)
+        self.model.fit_generator(datagen.flow(X_train, y_train, batch_size=batch_size), samples_per_epoch=len(X_train),
+                                 nb_epoch=epochs, verbose=1, validation_data=(X_val, y_val))
+
+    def save_model(self, architecture_filename, weights_filename):
+        model_json = self.model.to_json()
+        with open(architecture_filename, "w") as json_file:
+            json_file.write(model_json)
+
+        self.model.save_weights(weights_filename)
