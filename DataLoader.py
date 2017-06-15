1c24cafe2fa7880e1fd76d239dec722787a54ed9
Sebastian Pawlak <Sebastian.Pawlak@it.integro.pl>
2017-06-15 20:07:56 +0200

    learning

--- DataLoader.py
+++ DataLoader.py
@@ -2,6 +2,8 @@ import os, sys
 from skimage import data, io
 from PIL import Image
 import numpy as np
+import random
+from sklearn.feature_extraction import image
 
 
 def get_images_names_list(images_path, labels_path):
@@ -18,6 +20,28 @@ def get_image(path):
         return_image = np.resize(image, (image.shape[0], image.shape[1], 1))
         return return_image
     return image
+
+
+def split_image(picture_window_size, input_image, output_image, set_size):
+    half_window_size = int(picture_window_size/2)
+    patches = []
+    labels = []
+    #all patches- too big
+    # for i in range(half_window_size, len(input_image) - 1 - half_window_size):
+    #     for j in range(half_window_size , len(input_image[0]) - 1 - half_window_size):
+    #         patch = input_image[i-half_window_size:i+half_window_size+1, j-half_window_size:j+half_window_size+1]
+    #         label = np.resize(output_image[i][j], (1,1,1))
+    #         patches.append(patch)
+    #         labels.append(label)
+    for x in range(set_size):
+        i = random.randint(half_window_size, len(input_image) - 1 - half_window_size)
+        j = random.randint(half_window_size, len(input_image[0]) - 1 - half_window_size)
+        patch = input_image[i - half_window_size:i + half_window_size + 1,
+                j - half_window_size:j + half_window_size + 1]
+        label = np.resize(output_image[i][j], (1, 1, 1))
+        patches.append(patch)
+        labels.append(label)
+    return np.array(patches), np.array(labels)
 
 
 def show_image(image):
--- Learning.py
+++ Learning.py
@@ -6,37 +6,24 @@ seed = 7
 np.random.seed(seed)
 
 
-# DataLoader.show_image(labels[i])
-# DataLoader.show_image(train_pictures[i])
-
-def learn(images_dir, labels_dir, trainig_set_size, start_learning_example_number, stop_learning_example_number,
+def learn(images_dir, labels_dir, window_size, patch_size, start_learning_example_number, stop_learning_example_number,
           batch_size, epochs, model_name, model_weights_name):
-    #load data
+    # load data
     data = DataLoader.get_images_names_list(images_dir, labels_dir)
-    train_pictures = []
-    labels = []
 
-    #load current model
+    # load current model
     model = Model.Model()
     model.init_model()
     model.load_weights(model_weights_name)
 
-    #learn
+    # learn
     for i in range(start_learning_example_number, stop_learning_example_number):
-        if i % trainig_set_size == 0 and i > 0:
-            print("learning")
-            model.train_model(train_pictures, labels, batch_size, epochs)
-            train_pictures = []
-            labels = []
         print("pic num: {0}, name: {1}".format(i, data[i][0]))
-        train_pictures.append(DataLoader.get_image(data[i][1]))
-        labels.append(DataLoader.get_image(data[i][2]))
-
-    if len(train_pictures) > 0:
-        model.train_model((train_pictures, labels, batch_size, epochs))
-
-    #save model
-    model.save_model(model_name, model_weights_name)
+        image = DataLoader.get_image(data[i][1])
+        label = DataLoader.get_image(data[i][2])
+        train_pictures, labels = DataLoader.split_image(window_size, image, label, patch_size)
+        model.train_model(train_pictures, labels, batch_size, epochs)
+        model.save_model(model_name, model_weights_name)
 
 
 def main():
@@ -45,13 +32,16 @@ def main():
     learning_labels_path = 'D:/piro/piro3/labels/'
     model_name = "net_1"
     model_weights_name = "net_1_weights_1"
-    trainig_set_size = 6
+    window_size = 31  # must be odd
+    patch_size = 150
     start = 0
-    stop = 12
-    batch_size = 3
+    stop = 1
+    batch_size = 30
     epochs = 1
 
-    learn(learning_files_path, learning_labels_path, trainig_set_size, start, stop, batch_size, epochs, model_name, model_weights_name)
+    learn(learning_files_path, learning_labels_path, window_size, patch_size, start, stop, batch_size, epochs,
+          model_name,
+          model_weights_name)
 
 
 if __name__ == '__main__':
