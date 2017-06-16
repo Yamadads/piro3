f80b90a1d50aad3cd5cb393d9ed2589e6a189f30
uHappyLogic <kosiak.lukasz@gmail.com>
2017-06-16 02:19:50 +0200

    Filtering network added

---
+++ FilterLearning.py
@@ -0,0 +1,49 @@
+import FilteringDataLoader
+import numpy as np
+import FilteringModel
+
+seed = 7
+np.random.seed(seed)
+
+
+def learn(images_dir, labels_dir, window_size, patches_count, start_learning_example_number, stop_learning_example_number,
+          batch_size, epochs, model_name, model_weights_name):
+    # load data
+    data = FilteringDataLoader.get_images_names_list(images_dir, labels_dir)
+
+    # load current model
+    model = FilteringModel.FilteringModel()
+    model.init_model()
+    model.load_weights(model_weights_name)
+
+    # learn
+    for i in range(start_learning_example_number, stop_learning_example_number):
+        print("pic num: {0}, name: {1}".format(i, data[i][0]))
+        image = FilteringDataLoader.get_image(data[i][1], 200)
+        label = FilteringDataLoader.get_image(data[i][2], 200)
+        train_pictures, labels = FilteringDataLoader.split_image(window_size, image, label, patches_count)
+        model.train_model(train_pictures, labels, batch_size, epochs)
+        model.save_model(model_name, model_weights_name)
+
+
+def main():
+    learning_set_count = 1108
+    learning_files_path = './pictures/'
+    learning_labels_path = './labels/'
+    model_name = "filter_net_1"
+    model_weights_name = "filter_net_1_weights_1"
+    window_size = 11  # must be odd
+    patches_count = 3000
+    start = 0
+    stop = 1
+    batch_size = 30
+    epochs = 3
+
+    learn(learning_files_path, learning_labels_path, window_size, patches_count, start, stop, batch_size, epochs,
+          model_name,
+          model_weights_name)
+
+
+if __name__ == '__main__':
+    print("FilterLearning started")
+    main()
---
+++ FilteringDataLoader.py
@@ -0,0 +1,59 @@
+import os
+import random
+
+import cv2 as cv
+import numpy as np
+from PIL import Image
+from skimage import data
+
+
+def get_images_names_list(images_path, labels_path):
+    names_list = os.listdir(images_path)
+    full_path_names = [[os.path.splitext(x)[0], images_path + x, labels_path + os.path.splitext(x)[0] + '.tif'] for x in
+                       names_list]
+    return full_path_names
+
+
+def get_image(path, compress_to_size):
+    needed_channels = 3
+    image = data.imread(path, False)
+    if len(image.shape) != needed_channels:
+        return_image = np.resize(image, (image.shape[0], image.shape[1], 1))
+        image = return_image
+
+    image2 = cv.resize(image, (compress_to_size, compress_to_size))
+
+    # show_image(image2)
+
+    return image2
+
+
+def split_image(picture_window_size, input_image, output_image, set_size):
+    half_window_size = int(picture_window_size/2)
+    patches = []
+    labels = []
+
+    for x in range(set_size):
+        i = random.randint(half_window_size, len(input_image) - 1 - half_window_size)
+        j = random.randint(half_window_size, len(input_image[0]) - 1 - half_window_size)
+
+        patch = input_image[
+                i - half_window_size:i + half_window_size + 1,
+                j - half_window_size:j + half_window_size + 1
+        ]
+
+        label_patch = output_image[
+                i - half_window_size:i + half_window_size + 1,
+                j - half_window_size:j + half_window_size + 1
+        ]
+
+        label = np.resize([1 if np.sum(label_patch) > 0 else 0], (1, 1, 1))
+        patches.append(patch)
+        labels.append(label)
+
+    return np.array(patches), np.array(labels)
+
+
+def show_image(image):
+    im = Image.fromarray(image)
+    im.show()
